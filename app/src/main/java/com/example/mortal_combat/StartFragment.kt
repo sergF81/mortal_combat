93af1e4ab82bbeae6ec52325bc81e6f8a7fc0f94
Sergey Fortis <s_fortis@mail.ru>
2023-10-09 20:52:12 +0500

    Test task is done.

--- app/src/main/java/com/example/mortal_combat/StartFragment.kt
+++ app/src/main/java/com/example/mortal_combat/StartFragment.kt
@@ -7,8 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.fragment.navArgs
-import com.example.mortal_combat.databinding.FragmentEndBinding
 import com.example.mortal_combat.databinding.FragmentStartBinding
 
 
